eb07934376ee4aeb5816af1cdb7d09202aa91cfb
Elise Mery <elisemery28@gmail.com>
2022-12-12 11:55:16 +0100

    primary button style

---
+++ styles/primaryButton.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const PrimaryButtonstyles = StyleSheet.create({
+
+    primaryButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        width: '80%',
+        height: '10%',
+        backgroundColor: '#ec6e5b',
+        borderRadius: 10,
+        margin: 5,
+    },
+    textPrimaryButton: {
+        color: '#ffffff',
+        fontWeight: '500',
+        letterSpacing: 0.75,
+        fontSize: 14,
+        textTransform: 'uppercase'
+    },
+})
+
