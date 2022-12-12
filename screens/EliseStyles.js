90152632ac79ab9a98e70d96e13e8fdf285d666b
Elise Mery <elisemery28@gmail.com>
2022-12-12 15:27:24 +0100

    input styles

--- screens/EliseStyles.js
+++ screens/EliseStyles.js
@@ -1,14 +1,65 @@
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, TextInput } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 import { primaryButtonStyles } from '../styles/primaryButton';
 import { secondaryButtonStyles } from '../styles/secondaryButton';
 import { primaryButtonSmallStyles } from '../styles/primaryButtonSmall';
 import { secondaryButtonSmallStyles } from '../styles/secondaryButtonSmall';
+import { searchInputStyles } from '../styles/searchInput';
+import { dropdownStyles } from '../styles/dropdown';
+
+// const myTheme = require('../styles/darkDropdownTheme');
+
+// DropDownPicker.addTheme("darkDropdownTheme", myTheme);
+// DropDownPicker.setTheme("darkDropdownTheme");
+
+import { useState } from 'react';
+import React from 'react';
 
 export default function EliseStylesScreen({ navigation }) {
 
+
+    const [textInputValue, setTextInputValue] = useState('');
+    const [searchIsFocused, setSearchIsFocused] = useState(false);
+
+
+
+    DropDownPicker.setTheme("DARK");
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Apple', value: 'apple' },
+        { label: 'Banana', value: 'banana' },
+        { label: 'Pear', value: 'pear' },
+        { label: 'Kiwi', value: 'kiwi' },
+        { label: 'Ananas', value: 'ananas' },
+    ]);
+
+    //To get the items selected : value
+
+
     return (
         <View style={styles.container}>
-            <Text>Elise Styles</Text>
+            <DropDownPicker
+                style={dropdownStyles.header}
+                textStyle={dropdownStyles.text}
+                containerStyle={dropdownStyles.container}
+                multiple={true}
+                open={open}
+                value={value}
+                items={items}
+                setOpen={setOpen}
+                setValue={setValue}
+                setItems={setItems}
+            />
+
+            <TextInput style={searchIsFocused ? searchInputStyles.inputFocus : searchInputStyles.input}
+                onChangeText={text => setTextInputValue(text)}
+                value={textInputValue}
+                placeholder="Where ?"
+                placeholderTextColor="#7E8284"
+                onBlur={() => setSearchIsFocused(false)}
+                onFocus={() => setSearchIsFocused(true)}
+            />
 
             <TouchableOpacity style={primaryButtonStyles.button} activeOpacity={0.8}>
                 <Text style={primaryButtonStyles.buttonText}>Primary Button</Text>
@@ -32,7 +83,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#3A474E'
+        backgroundColor: '#272D31'
     },
 
 })
---
+++ styles/dropdown.js
@@ -0,0 +1,68 @@
+import { StyleSheet } from 'react-native';
+
+// TO USE THIS STYLE :
+
+// import { dropdownStyles } from '../styles/dropdown';
+
+// DropDownPicker.setTheme("DARK");
+// const [open, setOpen] = useState(false);
+// const [value, setValue] = useState(null);
+// const [items, setItems] = useState([
+//     { label: 'Apple', value: 'apple' },
+//     { label: 'Banana', value: 'banana' },
+//     { label: 'Pear', value: 'pear' },
+//     { label: 'Kiwi', value: 'kiwi' },
+//     { label: 'Ananas', value: 'ananas' },
+// ]);
+
+//To get the items selected : value
+
+//  <DropDownPicker
+//     style={dropdownStyles.header}
+//     textStyle={dropdownStyles.text}
+//     containerStyle={dropdownStyles.container}
+//     multiple={true}
+//     open={open}
+//     value={value}
+//     items={items}
+//     setOpen={setOpen}
+//     setValue={setValue}
+//     setItems={setItems}
+// /> 
+
+
+// WORK IN PROGRESS : CUSTOM THEME
+
+// const myTheme = require('../styles/darkDropdownTheme');
+
+// DropDownPicker.addTheme("darkDropdownTheme", myTheme);
+// DropDownPicker.setTheme("darkDropdownTheme");
+
+export const dropdownStyles = StyleSheet.create({
+    header: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#3A474E',
+        height: 50,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+        borderLeftWidth: 0,
+        borderTopWidth: 0,
+        borderRightWidth: 0,
+        borderBottomColor: '#7E8284',
+        borderBottomWidth: 1,
+        paddingLeft: 10,
+    },
+    text: {
+        fontSize: 20,
+        color: '#7E8284',
+    },
+    container: {
+        width: '80%',
+        margin: 5
+    },
+
+})
+
