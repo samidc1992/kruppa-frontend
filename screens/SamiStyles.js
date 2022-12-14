dc4bea7741db64c8b5c5a21e8e928e03d9dfca23
Sami <s.david.correa@gmail.com>
2022-12-14 16:13:03 +0100

    created top bar component

--- screens/SamiStyles.js
+++ screens/SamiStyles.js
@@ -1,9 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
-import TrippleTab from '../components/TrippleTab'
+import TrippleTab from '../components/TrippleTab';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import TopBar from '../components/TopBar';
 
 export default function SamiStylesScreen({ navigation }) {
     return (
         <View style={styles.container}>
+            <TopBar
+                text="Back to the roots"
+            />
             <TrippleTab 
             textTabLeft="search"
             textTabMiddle="sign in"
@@ -19,8 +24,6 @@ export default function SamiStylesScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#272D31'
     },
