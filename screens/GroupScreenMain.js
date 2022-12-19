af7c2850d7b6265b885d61b25f6fa88fd7c6c360
Sami <s.david.correa@gmail.com>
2022-12-19 18:11:46 +0100

    updated join button status

--- screens/GroupScreenMain.js
+++ screens/GroupScreenMain.js
@@ -43,27 +43,24 @@ export default function GroupScreenMain({ navigation }) {
                     })
                 }
             })
-    }, [joined])
+    }, [])
 
     useEffect(() => {
         if (user.token) {
-            fetch(`${BACKEND_ADDRESS}/users/join-group`, {
-                method: 'PUT',
+            fetch(`${BACKEND_ADDRESS}/users/join-status`, {
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ group_id, token: user.token }),
             }).then(response => response.json())
             .then(data => {
-                console.log('charge', data)
-                if (data.result) {
-                    setJoined(false);
-                } else {
+                if (!data.result) {
                     setJoined(true);
-                }
-            })
+                } 
+            });
         } 
-    }, [])
+    }, []);
 
     function handleJoinGroup() {
         if (user.token) {
@@ -75,7 +72,6 @@ export default function GroupScreenMain({ navigation }) {
                 body: JSON.stringify({ group_id, token: user.token }),
             }).then(response => response.json())
                 .then(data => {
-                    console.log('join', data)
                     if (data.result) {
                         setJoined(true);
                     }
@@ -95,7 +91,6 @@ export default function GroupScreenMain({ navigation }) {
                 body: JSON.stringify({ group_id, token: user.token }),
             }).then(response => response.json())
                 .then(data => {
-                    console.log('leave', data)
                     if (data.result) {
                         setJoined(false);
                     } else {
