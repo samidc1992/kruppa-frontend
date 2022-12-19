735ed5087e0ab495c96e2b80f6fc5f93a0d152b3
Elise Mery <elisemery28@gmail.com>
2022-12-19 17:50:14 +0100

    remove test from welcome screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -48,9 +48,9 @@ export default function WelcomeScreen({ navigation }) {
                 {/* <PrimaryButtonSmall 
                 text='Profile Signup' 
                 onPress={() => navigation.navigate('SignUpProfile')}/> */}
-                <PrimaryButtonSmall
+                {/* <PrimaryButtonSmall
                     text='Elise tests'
-                    onPress={() => navigation.navigate('Elise')} />
+                    onPress={() => navigation.navigate('Elise')} /> */}
                 {/* <PrimaryButtonSmall
                     text='Create group'
                     onPress={() => navigation.navigate('GroupCreation')} /> */}
