209693fdd2609aaee9d137a9976a4f228abb8d8a
Elise Mery <elisemery28@gmail.com>
2022-12-20 14:53:20 +0100

    change backend address

--- backendAdress.js
+++ backendAdress.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const BACKEND_ADDRESS = 'http://192.168.10.146:3000'
-=======
-export const BACKEND_ADDRESS = 'http://192.168.10.163:3000'
->>>>>>> 4860dd4a792a91112f002c0c8adbcd258b3aeabe
